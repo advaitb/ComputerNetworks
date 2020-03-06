d75289e4c36030f3bcd90df480c818518a43cdb9
shloksobti <shlok.sobti@gmail.com>
2020-03-06 15:47:06 -0600

    wip

--- project2/sendfile.c
+++ project2/sendfile.c
@@ -3,28 +3,48 @@
 #include <string.h>
 #include <unistd.h>
 
+#include <sys/time.h>
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
 
+
+#define MAX_FILE_SIZE 30000000
+
 int main(int argc, char const *argv[])
 {    
     /* DGRAM Packet Structure:
      * Packet Type:  
-     * Ack Number:
-     * Advertised Window
-     * Data
+     * Identifier: 
+     * Advertised Window:
+     * Data: 
+     * CRC Error Code: 
      */
+
+    /* TODO:
+     * Add Stop and Wait Ack Scheme -> Replace with Sliding Window Later
+     * Error Checking Codes -> Cyclic Redundancy Check
+     */
+
     unsigned short port;
     int sockfd;
     char rcv_buffer[1024];
     char *msg;
     unsigned int server_address;
     struct sockaddr_in s_in;
+    
 
-    // 1. Create a UDP Socket
+    long lSize;
+    char *file_data;
+
+    struct timeval tv;
+    tv.tv_sec = 1;
+    tv.tv_usec = 0;
+
+    // Create a UDP Socket
     if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
     {
         perror("Socket creation failed");
@@ -34,18 +54,63 @@ int main(int argc, char const *argv[])
     s_in.sin_family = AF_INET;
     s_in.sin_port = htons(port);
     s_in.sin_addr.s_addr = server_address;
+    setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv, sizeof tv); // Set the timeout value
+
+
+
+    // Read the FILE and breakdown to package into PACKETS.
+    int packet_size;
+    FILE *fp;
+    unsigned char buf[MAX_FILE_SIZE];
+    fp = fopen("test.bin", "rb");
+    if (!fp)
+    {
+        printf("Unable to open file.\n");
+        return 1;
+    }
+    printf("File open succeeded.\n");
+    fseek(fp, 0, SEEK_END);
+    lSize = ftell(fp);
+    rewind(fp);
+    file_data = (char *)malloc(lSize* sizeof(char));
+              
+    if (file_data)
+    {
+        size_t result = fread(file_data, 1, lSize, fp);
+        if (result!=lSize)
+            perror("Error reading file");
+            return 1;
+    }
+
+
+
+
+
+
+
+    // Send Message to Server
+    int send_cnt = 0;
+
+    // While ALL packets are not sent
+    while (1)
+    {
+        // While EACH packet is not sent
+        while (send_cnt != packet_size)
+        {
+            int tmp_cnt = sendto(sockfd, (const char *)msg, strlen(msg), NULL, (const struct sockaddr *) &s_in, sizeof(s_in));
+            send_cnt += tmp_cnt;
+        }
+
+        // Wait until you hear ACK for last sent package
+        int len;
+        recvfrom(sockfd, (const char *)rcv_buffer, sizeof(rcv_buffer), MSG_WAITALL, (struct sockaddr *)&s_in, len); // If ACK is not recvd it will timeout!
+        // Check if this ack is for Packet
+        
+        
+        
+    }
     
-
-    // 2. Send Message to Server
-    int send_cnt = sendto(sockfd, (const char *)msg, strlen(msg), MSG_CONFIRM, (const struct sockaddr *) &s_in, sizeof(s_in));
-    if (send_cnt < 0)
-        perror("Datagram could not be sent!");
-
-    printf("Datagram Sent.\n");
-
-    // 3. Wait until ACK from server is received
-    int n, len;
-    n = recvfrom(sockfd, (const char *)rcv_buffer, sizeof(rcv_buffer), MSG_WAITALL, (struct sockaddr *)&s_in, len);
+    
 
     // 4. Process reply, and return to Step 2
 
