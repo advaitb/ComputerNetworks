e5c0ecdaf8cdcb3f6a76ba3cc81af45844cce2e5
shloksobti <shlok.sobti@gmail.com>
2020-03-06 14:29:22 -0600

    use stop and wait for now - once this works change to sliding window

--- project2/sendfile.c
+++ project2/sendfile.c
@@ -25,7 +25,7 @@ int main(int argc, char const *argv[])
     struct sockaddr_in s_in;
 
     // 1. Create a UDP Socket
-    if ((sockfd == socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
     {
         perror("Socket creation failed");
         exit(EXIT_FAILURE);
@@ -34,17 +34,19 @@ int main(int argc, char const *argv[])
     s_in.sin_family = AF_INET;
     s_in.sin_port = htons(port);
     s_in.sin_addr.s_addr = server_address;
-
-
+    
 
     // 2. Send Message to Server
     int send_cnt = sendto(sockfd, (const char *)msg, strlen(msg), MSG_CONFIRM, (const struct sockaddr *) &s_in, sizeof(s_in));
+    if (send_cnt < 0)
+        perror("Datagram could not be sent!");
+
     printf("Datagram Sent.\n");
 
-    // 3. Wait until response from server is received
+    // 3. Wait until ACK from server is received
     int n, len;
     n = recvfrom(sockfd, (const char *)rcv_buffer, sizeof(rcv_buffer), MSG_WAITALL, (struct sockaddr *)&s_in, len);
-    
+
     // 4. Process reply, and return to Step 2
 
     // 5. Close socket descriptor and exit.
