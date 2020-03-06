fdfebfd16f5e091005fe799a2dd4443190e44bb5
shloksobti <shlok.sobti@gmail.com>
2020-03-06 12:20:23 -0600

    init project2

---
+++ project2/sendfile.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+int main(int argc, char const *argv[])
+{
+    return 0;
+}
