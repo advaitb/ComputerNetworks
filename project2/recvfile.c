aec1e882c4a9cd67addae0c9b6df954f35532de2
TianyangPan <1045380922@qq.com>
2020-03-08 12:45:05 -0500

    initial commit

--- project2/recvfile.c
+++ project2/recvfile.c
@@ -11,5 +11,36 @@
 
 int main(int argc, char const *argv[])
 {
-    return 0;
+
+    unsigned short server_port;
+    struct sockaddr_in sin;
+
+    // Create a UDP Socket
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    /* fill in the address of the server socket */
+    memset (&sin, 0, sizeof (sin));
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = INADDR_ANY;
+    sin.sin_port = htons(server_port);
+
+    /* bind server socket to the address */
+    if (bind(sock, (struct sockaddr *) &sin, sizeof (sin)) < 0)
+    {
+        perror("binding socket to address");
+        abort();
+    }
+
+
+
+
+
+
+
+
+        return 0;
 }
