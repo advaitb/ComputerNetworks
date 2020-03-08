facee030dccfe7c6b919e0f1c5a4fb822e64e84b
TianyangPan <1045380922@qq.com>
2020-03-08 13:54:26 -0500

    Fix naming issue

--- project2/recvfile.c
+++ project2/recvfile.c
@@ -13,7 +13,13 @@ int main(int argc, char const *argv[])
 {
 
     unsigned short server_port;
-    struct sockaddr_in sin;
+    struct sockaddr_in sin, addr;
+    int sockfd, new_sock;
+    socklen_t addr_len = sizeof(struct sockaddr_in);
+
+    // Maximum number of pending connection requests
+    int BACKLOG = 5;
+
 
     // Create a UDP Socket
     if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
@@ -29,15 +35,29 @@ int main(int argc, char const *argv[])
     sin.sin_port = htons(server_port);
 
     /* bind server socket to the address */
-    if (bind(sock, (struct sockaddr *) &sin, sizeof (sin)) < 0)
+    if (bind(sockfd, (struct sockaddr *) &sin, sizeof (sin)) < 0)
     {
         perror("binding socket to address");
         abort();
     }
 
+    // Put the socket in listen mode
+    if (listen(sockfd, BACKLOG) < 0)
+    {
+        perror("listen on socket failed");
+        abort();
+    }
 
-
-
+    // Keep waiting for incoming connection
+    while(1)
+    {
+        new_sock = accept(sockfd, (struct sockaddr *) &addr, &addr_len);
+        if (new_sock < 0)
+        {
+            perror("error accepting connection");
+            abort();
+        }
+    }
 
 
 
