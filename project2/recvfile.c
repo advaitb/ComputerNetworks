bf1d8dc8c0ce8215d67fff9b07e5205f7d0e96cf
TianyangPan <1045380922@qq.com>
2020-03-18 11:12:15 -0500

    Fix listen error

--- project2/recvfile.c
+++ project2/recvfile.c
@@ -40,12 +40,8 @@ int main(int argc, char const *argv[])
     char* port = argv[2];
     unsigned short server_port = atoi(port);
     struct sockaddr_in sin, addr;
-    int sockfd, new_sock;
+    int sockfd;
     socklen_t addr_len = sizeof(struct sockaddr_in);
-
-    // Maximum number of pending connection requests
-    int BACKLOG = 5;
-
 
     // Create a UDP Socket
     if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
@@ -63,14 +59,7 @@ int main(int argc, char const *argv[])
     /* bind server socket to the address */
     if (bind(sockfd, (struct sockaddr *) &sin, sizeof(sin)) < 0)
     {
-        perror("binding socket to address");
-        exit(EXIT_FAILURE);
-    }
-
-    // Put the socket in listen mode
-    if (listen(sockfd, BACKLOG) < 0)
-    {
-        perror("listen on socket failed");
+        perror("Failed to bind socket to address");
         exit(EXIT_FAILURE);
     }
 
@@ -83,96 +72,96 @@ int main(int argc, char const *argv[])
     char fileName[20];
     char recv_msg[packet_size - HEADER_LEN];
 
-    // Keep waiting for incoming connection
+    // // Keep waiting for incoming connection
+    // while(1)
+    // {
+    // Accept a new socket
+    // new_sock = accept(sockfd, (struct sockaddr *) &addr, &addr_len);
+    // if (new_sock < 0)
+    // {
+    //     perror("error accepting connection");
+    //     abort();
+    // }
+
+    // // Connection is made
+    // printf("Accepted connection. Client IP address: %s\n", inet_ntoa(addr.sin_addr));
+
+    FILE *fp;
+
+    ackmsg[0] = 1;
+
+    char lastID[1];
+    lastID[0] = 1;
+    // Receive all the packets
     while(1)
     {
-        // Accept a new socket
-        new_sock = accept(sockfd, (struct sockaddr *) &addr, &addr_len);
-        if (new_sock < 0)
+        recv_buf = (char *)malloc(packet_size);
+        int count = recvfrom(sockfd, recv_buf, sizeof(recv_buf), MSG_WAITALL, (struct sockaddr *)&addr, &addr_len);
+        int tempcount = 0;
+        while (count < packet_size)
         {
-            perror("error accepting connection");
-            abort();
-        }
-
-        // Connection is made
-        printf("Accepted connection. Client IP address: %s\n", inet_ntoa(addr.sin_addr));
-
-        FILE *fp;
-
-        ackmsg[0] = 1;
-
-        char lastID[1];
-        lastID[0] = 1;
-        // Receive all the packets
-        while(1)
-        {
-            recv_buf = (char *)malloc(packet_size);
-            int count = recvfrom(new_sock, recv_buf, sizeof(recv_buf), MSG_WAITALL, (struct sockaddr *)&addr, &addr_len);
-            int tempcount = 0;
-            while (count < packet_size)
-            {
-                tempcount = recvfrom(new_sock, recv_buf+count, sizeof(recv_buf)-count, MSG_WAITALL, (struct sockaddr *)&addr, &addr_len);
-                if (tempcount == -1)
-                    continue;
-                count += tempcount;
-            }
-            // Check crc
-            unsigned int crc = crc32b(recv_buf);
-            if (crc != 0)
-            {
-                free(recv_buf);
-                continue; // If error detected, discard the packet
-            }
-
-            // Check sequence number in stop & wait fashion
-            char recvID = recv_buf[1];
-            if (recvID != lastID[0])
-                lastID[0] = recvID;
-            else
-            {
-                free(recv_buf);
+            tempcount = recvfrom(sockfd, recv_buf+count, sizeof(recv_buf)-count, MSG_WAITALL, (struct sockaddr *)&addr, &addr_len);
+            if (tempcount == -1)
                 continue;
-            }
-
-            // Copy the packet to the message
-            // memcpy(recv_msg, &recv_buf, packet_size);
-            strncpy(dir, recv_buf+2, 50);
-            strncpy(fileName, recv_buf+52, 20);
-            strncpy(recv_msg, recv_buf+72, 1000);
-
-            // Save message to the file
-            char filePath[70];
-            strcpy(filePath, dir);
-            strcat(filePath, fileName);
-            fp = fopen(filePath, "a");
-            if (!fp)
-            {
-                printf("Unable to open file.\n");
-                return 1;
-            }
-            if (fwrite(recv_msg, 1, sizeof(recv_msg), fp) != 1)
-            {
-                printf("Write to file error!\n");
-                exit(1);
-            }
-
-            fclose(fp);
-            free(recv_msg);
-
-            // Send Ack
-            ackmsg[1] = lastID[0];
-            int sendcount = sendto(new_sock, (const char *)ackmsg, sizeof(ackmsg), 0, (const struct sockaddr *) &addr, sizeof(addr));
-            if (sendcount <= 0)
-            {
-                printf("Error sending!\n");
-                return 1;
-            }
-
+            count += tempcount;
+        }
+        // Check crc
+        unsigned int crc = crc32b(recv_buf);
+        if (crc != 0)
+        {
             free(recv_buf);
+            continue; // If error detected, discard the packet
         }
 
+        // Check sequence number in stop & wait fashion
+        char recvID = recv_buf[1];
+        if (recvID != lastID[0])
+            lastID[0] = recvID;
+        else
+        {
+            free(recv_buf);
+            continue;
+        }
 
+        // Copy the packet to the message
+        // memcpy(recv_msg, &recv_buf, packet_size);
+        strncpy(dir, recv_buf+2, 50);
+        strncpy(fileName, recv_buf+52, 20);
+        strncpy(recv_msg, recv_buf+72, 1000);
+
+        // Save message to the file
+        char filePath[70];
+        strcpy(filePath, dir);
+        strcat(filePath, fileName);
+        fp = fopen(filePath, "a");
+        if (!fp)
+        {
+            printf("Unable to open file.\n");
+            return 1;
+        }
+        if (fwrite(recv_msg, 1, sizeof(recv_msg), fp) != 1)
+        {
+            printf("Write to file error!\n");
+            exit(1);
+        }
+
+        fclose(fp);
+        free(recv_msg);
+
+        // Send Ack
+        ackmsg[1] = lastID[0];
+        int sendcount = sendto(sockfd, (const char *)ackmsg, sizeof(ackmsg), MSG_CONFIRM, (const struct sockaddr *) &addr, sizeof(addr));
+        if (sendcount <= 0)
+        {
+            printf("Error sending!\n");
+            return 1;
+        }
+
+        free(recv_buf);
     }
+
+
+    // }
 
 
 
