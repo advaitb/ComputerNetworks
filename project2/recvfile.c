96f9797b29524261af440e17ab890cf97592a8e2
shloksobti <shlok.sobti@gmail.com>
2020-03-27 21:36:37 -0500

    wip

--- project2/recvfile.c
+++ project2/recvfile.c
@@ -187,14 +187,14 @@ int main(int argc, char *argv[])
         {
             free(recv_buf);
             // lastID = recvID;
-            if (recvID == 0)
-            {
-                lastID = 1;
-            }
-            else if (recvID == 1)
-            {
-                lastID = 0;
-            }
+            // if (recvID == 0)
+            // {
+            //     lastID = 1;
+            // }
+            // else if (recvID == 1)
+            // {
+            //     lastID = 0;
+            // }
             printf("[recv data] %d %u IGNORED\n", (total_data - msg_size), msg_size);
 
             // In this case an ACK is still sent
