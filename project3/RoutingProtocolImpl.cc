3dbdaa487c733a37035f01a5f016c1740393e67b
advaitb <adsgarfield95@gmail.com>
2020-03-29 22:40:40 -0500

    added alarm definitions

--- project3/RoutingProtocolImpl.cc
+++ project3/RoutingProtocolImpl.cc
@@ -1,4 +1,5 @@
 #include "RoutingProtocolImpl.h"
+#include <string.h>
 
 RoutingProtocolImpl::RoutingProtocolImpl(Node *n) : RoutingProtocol(n) {
   sys = n;
@@ -15,10 +16,22 @@ void RoutingProtocolImpl::init(unsigned short num_ports, unsigned short router_i
 
 void RoutingProtocolImpl::handle_alarm(void *data) {
   // add your own code
+  char* alarmtype = reinterpret_cast<char*>(data);
+  if (strcmp(alarmtype,"ping") == 0) pingTime();
+  else if(strcmp(alarmtype, "pong") == 0) pongTime();
+  else if(strcmp(alarmtype, "distancevector") == 0) dvTime();
+  else if(strcmp(alarmtype, "linkstatepacket") == 0) lsTime();
+  else if(strcmp(alarmtype, "update") == 0) updateTime();
+  else {
+	fprintf(stderr, "Not able to recognize %s alarm", alarmtype);
+	exit(EXIT_FAILURE);
+  }
 }
 
 void RoutingProtocolImpl::recv(unsigned short port, void *packet, unsigned short size) {
   // add your own code
+  // need to free recv after processing
+  // need to check what kind of packet has been received - there are 5 different kinds of packets!
 }
 
 // add more of your own code
