066cdfaa73f6a7ca46647f2b875190c70d5b8eba
Advait Balaji <adsgarfield95@gmail.com>
2020-03-31 16:30:46 -0500

    remove pongtime

--- project3/RoutingProtocolImpl.cc
+++ project3/RoutingProtocolImpl.cc
@@ -39,10 +39,9 @@ void RoutingProtocolImpl::init(unsigned short num_ports, unsigned short router_i
 void RoutingProtocolImpl::handle_alarm(void *data) {
   char* alarmtype = reinterpret_cast<char*>(data);
   if (strcmp(alarmtype,this->ping) == 0) pingTime();
-  else if(strcmp(alarmtype, this->pong) == 0) pongTime();
+  else if(strcmp(alarmtype, this->update) == 0) updateTime();
   else if(strcmp(alarmtype, this->distancevector) == 0) dvTime();
   else if(strcmp(alarmtype, this->linkstate) == 0) lsTime();
-  else if(strcmp(alarmtype, this->update) == 0) updateTime();
   else {
 	fprintf(stderr, "Not able to recognize %s alarm\n", alarmtype);
 	exit(EXIT_FAILURE);
